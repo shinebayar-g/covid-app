636e1b7bde629912b238587e37e1ac3064b7a75d
shinebayar-g <shinebayar.gansukh@gmail.com>
2020-11-18 03:43:29 -0500

    add initial WIP code

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+###
+# Development Only
+###
+
+mysql_healthy() {
+  (docker ps | grep mysql | grep healthy) >/dev/null 2>&1
+}
+
+echo "Starting mysql..."
+docker-compose up -d mysql
+
+printf "Waiting for mysql to become healthy "
+until mysql_healthy
+do
+  sleep 2
+  printf "."
+done
+printf " \x1b[32mdone\x1b[0m\n"
+
+echo "Bootstrapping the database..."
+docker-compose run portal bundle exec rake portal:bootstrap
